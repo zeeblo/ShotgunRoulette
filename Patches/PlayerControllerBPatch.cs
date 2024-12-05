34ba5b4a53e5b5f958360bc2e4037b8ca881f254
zeeblo <141220959+zeeblo@users.noreply.github.com>
2024-12-04 22:11:36 -0800

    wip roulette button

--- Patches/PlayerControllerBPatch.cs
+++ Patches/PlayerControllerBPatch.cs
@@ -8,17 +8,49 @@ namespace ShotgunRoulette.Patches
     internal class PlayerControllerBPatch
     {
 
-        [HarmonyPatch("Update")]
+
+        private static void CheckForGun(PlayerControllerB __instance)
+        {
+            if (__instance.currentlyHeldObjectServer != null && __instance.currentlyHeldObjectServer.itemProperties.itemName.ToLower().Contains("shotgun"))
+            {
+                //HUDManager.Instance.ClearControlTips();
+                HUDManager.Instance.controlTipLines.Last().text = $"{HUDManager.Instance.controlTipLines.Last().text} \n Roulette : [H]";
+            }
+
+        }
+
+        [HarmonyPatch(nameof(PlayerControllerB.Update))]
         [HarmonyPostfix]
-        private static void UpdatePatch()
+        private static void UpdatePatch(PlayerControllerB __instance)
         {
 
             if (Keyboard.current.digit1Key.wasPressedThisFrame)
             {
                 Plugin.SpawnShotgun();
             }
+
+            if (Keyboard.current.hKey.wasPressedThisFrame)
+            {
+                if (__instance.currentlyHeldObjectServer != null && __instance.currentlyHeldObjectServer.itemProperties.itemName.ToLower().Contains("shotgun"))
+                {
+                    __instance.currentlyHeldObjectServer.transform.localScale = new UnityEngine.Vector3(0.2f, 0.2f, -0.2f);
+                }
+            }
         }
 
+        [HarmonyPatch(nameof(PlayerControllerB.GrabObject))]
+        [HarmonyPostfix]
+        private static void BeginGrabObjectPatch(PlayerControllerB __instance)
+        {
+            CheckForGun(__instance);
+        }
+
+        [HarmonyPatch(nameof(PlayerControllerB.SwitchToItemSlot))]
+        [HarmonyPostfix]
+        private static void SwitchToItemSlotPatch(PlayerControllerB __instance)
+        {
+            CheckForGun(__instance);
+        }
 
     }
 }
